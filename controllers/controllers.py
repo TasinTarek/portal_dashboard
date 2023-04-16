af260705b5b3c16cbb2e34eda9f5d467edbf246e
TasinTarek <tasin.tarek13@gmail.com>
2023-04-16 16:24:19 +0600

    Relational Field Controller Done

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -49,157 +49,196 @@ class MyPortalController(http.Controller):
     @http.route('/my/admission/online/applications/list/form', auth='public', website=True)
     def update_form(self, **kw):
         student_info = request.env['se.application'].sudo().search([])
-
+        batch = request.env['se.batch'].sudo().search([])
+        shift = request.env['se.education.shift'].sudo().search([])
+        type = request.env['se.student.type'].sudo().search([])
+        semester = request.env['se.semester'].sudo().search([])
+        semester_type = request.env['se.semester.type'].sudo().search([])
         return request.render('smartedu_portal.se_student_admission_template', {
+            # Relational Fields:
             "student_info": student_info,
-
+            "batch" : batch, 
+            "type" : type, 
+            "shift" : shift,
+            "semester": semester,
+            "semester_type" : semester_type,
         })
 
     @http.route('/my/admission/online/applications/update', auth='public', website=True)
     def update(self, **kw):
         request.env.context.get('user_id')
-        # get professional skill
+        # get student info
         student_info = request.env['se.application'].sudo().search([])
 
         # update student info with new values
         student_info.first_name = kw["first_name"]
         student_info.middle_name = kw["middle_name"]
         student_info.last_name = kw["last_name"]
-        # student_info.student_id = kw["student_id", False]
-        # student_info.application_serial_number = kw["application_serial_number", False]
-        # student_info.application_number = kw["application_number", False]
-        # student_info.application_date = kw["application_date", False]
-        # student_info.admission_date = kw["admission_date", False]
-        # student_info.admission_expire_date = kw["admission_expire_date", False]
-        # student_info.batch_id = kw["batch_id", False]
-        # student_info.fees = kw["fees", False]
-        # student_info.admission_fee = kw["admission_fee", False]
-        # student_info.fees_term_id = kw["fees_term_id", False]
-        # student_info.order_id = kw["order_id", False]
-        # student_info.academic_faculty_id = kw["academic_faculty_id", False]
-        # student_info.department_id = kw["department_id", False]
-        # student_info.semester_id = kw["semester_id", False]
-        # student_info.semester_type_id = kw["semester_type_id", False]
-        # student_info.semester_year_string = kw["semester_year_string", False]
-        # student_info.form_apply_type = kw["form_apply_type", False]
-        # student_info.student_type = kw["student_type", False]
-        # student_info.form_apply_type = kw["form_apply_type", False]
-        # student_info.education_shift_id = kw["education_shift_id", False]
-        # student_info.campus_type = kw["campus_type", False]
-        # student_info.registration_number_ssc = kw["registration_number_ssc", False]
-        # student_info.roll_number_ssc = kw["roll_number_ssc", False]
-        # student_info.education_board_ssc_id = kw["education_board_ssc_id", False]
-        # student_info.year_ssc = kw["year_ssc", False]
-        # student_info.ssc_gpa = kw["ssc_gpa", False]
-        # student_info.ssc_certificate = kw["ssc_certificate", False]
-        # student_info.registration_number_hsc = kw["registration_number_hsc", False]
-        # student_info.roll_number_hsc = kw["roll_number_hsc", False]
-        # student_info.education_board_hsc_id = kw["education_board_hsc_id", False]
-        # student_info.year_hsc = kw["year_hsc", False]
-        # student_info.hsc_gpa = kw["hsc_gpa", False]
-        # student_info.hsc_certificate = kw["hsc_certificate", False]
-        # student_info.education_board_o_level_id = kw["education_board_o_level_id", False]
-        # student_info.passing_year_o_level = kw["passing_year_o_level", False]
-        # student_info.admission_board_result_o_level_ids = kw["admission_board_result_o_level_ids", False]
-        # student_info.education_board_a_level_id = kw["education_board_a_level_id", False]
-        # student_info.education_board_a_level_id = kw["education_board_a_level_id", False]
-        # student_info.passing_year_a_level = kw["passing_year_a_level", False]
-        # student_info.admission_board_result_a_level_ids = kw["admission_board_result_a_level_ids", False]
-        student_info.gender = kw["gender"]
-        # student_info.marital_status = kw["marital_status", False]
-        # student_info.religion = kw["religion", False]
-        # student_info.blood_group = kw["blood_group", False]
-        # student_info.email = kw["email", False]
-        # student_info.phone = kw["phone", False]
-        # student_info.emergency_contact_info = kw["emergency_contact_info", False]
-        # student_info.street = kw["street", False]
-        # student_info.zip = kw["zip", False]
-        # student_info.permanent_district_id = kw["permanent_district_id", False]
-        # student_info.country_id = kw["country_id", False]
-        # student_info.present_street = kw["present_street", False]
-        # student_info.present_zip = kw["present_zip", False]
-        # student_info.present_district_id = kw["present_district_id", False]
-        # student_info.present_country_id = kw["present_country_id", False]
-        # student_info.is_permanent_present_address_same = kw["is_permanent_present_address_same", False]
-        # student_info.whatsapp_number = kw["whatsapp_number", False]
-        # student_info.social_network = kw["social_network", False]
-        # student_info.nationality = kw["nationality", False]
-        # student_info.national_id_no = kw["national_id_no", False]
-        # student_info.passport_no = kw["passport_no", False]
-        # student_info.visa_no = kw["visa_no", False]
-        # student_info.nationalID = kw["nationalID", False]
-        # student_info.visa_no = kw["visa_no", False]
-        # student_info.visa_expire_date = kw["visa_expire_date", False]
-        # student_info.father_name = kw["father_name", False]
-        # student_info.father_contact_number = kw["father_contact_number", False]
-        # student_info.father_email = kw["father_email", False]
-        # student_info.father_national_id = kw["father_national_id", False]
-        # student_info.father_passport = kw["father_passport", False]
-        # student_info.father_birthday = kw["father_birthday", False]
-        # student_info.father_age = kw["father_age", False]
-        # student_info.father_occupation = kw["father_occupation", False]
-        # student_info.father_designation = kw["father_designation", False]
-        # student_info.father_annual_income = kw["father_annual_income", False]
-        # student_info.mother_name = kw["mother_name", False]
-        # student_info.mother_contact_number = kw["mother_contact_number", False]
-        # student_info.mother_email = kw["mother_email", False]
-        # student_info.mother_national_id = kw["mother_national_id", False]
-        # student_info.mother_passport = kw["mother_passport", False]
-        # student_info.mother_birthday = kw["mother_birthday", False]
-        # student_info.mother_age = kw["mother_age", False]
-        # student_info.mother_occupation = kw["mother_occupation", False]
-        # student_info.mother_designation = kw["mother_designation", False]
-        # student_info.local_guardian_name = kw["local_guardian_name", False]
-        # student_info.local_guardian_email = kw["local_guardian_email", False]
-        # student_info.local_guardian_contact_number = kw["local_guardian_contact_number", False]
-        # student_info.local_guardian_national_id = kw["local_guardian_national_id", False]
-        # student_info.local_guardian_passport = kw["local_guardian_passport", False]
-        # student_info.local_guardian_relation = kw["local_guardian_relation", False]
-        # student_info.local_guardian_street = kw["local_guardian_street", False]
-        # student_info.local_guardian_zip = kw["local_guardian_zip", False]
-        # student_info.local_guardian_state_id = kw["local_guardian_state_id", False]
-        # student_info.local_guardian_country_id = kw["local_guardian_country_id", False]
+        # student_info.student_id = kw["student_id"]
+        
+        # Update the many-to-one field (batch_id)
+        batch_id = int(kw.get('batch_id', False))
+        if batch_id:
+            batch = request.env['se.batch'].sudo().browse(batch_id)
+            student_info.batch_id = batch
+
+#     # Update the many-to-one field (student_type_id)
+        student_type_id = int(kw.get('student_type_id', False))
+        if student_type_id:
+            student_type = request.env['se.student.type'].sudo().browse(
+            student_type_id)
+            student_info.student_type_id = student_type
+    #  # Update the many-to-one field (semester_id)
+        semester_id = int(kw.get('semester_id', False))
+        if semester_id:
+            semester_id = request.env['se.semester'].sudo().browse(
+            semester_id)
+            student_info.semester_id = semester_id
+        
+        # Update the many-to-one field (semester_type_id)
+        semester_type_id = int(kw.get('semester_type_id', False))
+        if semester_type_id:
+            semester_type_id = request.env['se.semester.type'].sudo().browse(
+            semester_type_id)
+            student_info.semester_type_id = semester_type_id
+
+        # Update the many-to-one field (semester_type_id)
+        education_shift_id = int(kw.get('education_shift_id', False))
+        if education_shift_id:
+            education_shift_id = request.env['se.education.shift'].sudo().browse(
+                education_shift_id)
+            student_info.education_shift_id = education_shift_id
+
+        student_info.application_serial_number = kw["application_serial_number"]
+        student_info.application_number = kw["application_number"]
+        student_info.application_date = kw["application_date"]
+        student_info.admission_date = kw["admission_date"]
+        student_info.admission_expire_date = kw["admission_expire_date"]
+        
+        # student_info.fees = kw["fees"]
+        # student_info.admission_fee = kw["admission_fee"]
+        # student_info.fees_term_id = kw["fees_term_id"]
+        # student_info.order_id = kw["order_id"]
+        # student_info.academic_faculty_id = kw["academic_faculty_id"]
+        # student_info.department_id = kw["department_id"]
+     
+        # student_info.semester_year_string = kw["semester_year_string"]
+        # student_info.form_apply_type = kw["form_apply_type"]
+        
+        # student_info.form_apply_type = kw["form_apply_type"]
+        
+        # student_info.campus_type = kw["campus_type"]
+        # student_info.registration_number_ssc = kw["registration_number_ssc"]
+        # student_info.roll_number_ssc = kw["roll_number_ssc"]
+        # student_info.education_board_ssc_id = kw["education_board_ssc_id"]
+        # student_info.year_ssc = kw["year_ssc"]
+        # student_info.ssc_gpa = kw["ssc_gpa"]
+        # student_info.ssc_certificate = kw["ssc_certificate"]
+        # student_info.registration_number_hsc = kw["registration_number_hsc"]
+        # student_info.roll_number_hsc = kw["roll_number_hsc"]
+        # student_info.education_board_hsc_id = kw["education_board_hsc_id"]
+        # student_info.year_hsc = kw["year_hsc"]
+        # student_info.hsc_gpa = kw["hsc_gpa"]
+        # student_info.hsc_certificate = kw["hsc_certificate"]
+        # student_info.education_board_o_level_id = kw["education_board_o_level_id"]
+        # student_info.passing_year_o_level = kw["passing_year_o_level"]
+        # student_info.admission_board_result_o_level_ids = kw["admission_board_result_o_level_ids"]
+        # student_info.education_board_a_level_id = kw["education_board_a_level_id"]
+        # student_info.education_board_a_level_id = kw["education_board_a_level_id"]
+        # student_info.passing_year_a_level = kw["passing_year_a_level"]
+        # student_info.admission_board_result_a_level_ids = kw["admission_board_result_a_level_ids"]
+        # student_info.gender = kw["gender"]
+        # student_info.marital_status = kw["marital_status"]
+        # student_info.religion = kw["religion"]
+        # student_info.blood_group = kw["blood_group"]
+        # student_info.email = kw["email"]
+        # student_info.phone = kw["phone"]
+        # student_info.emergency_contact_info = kw["emergency_contact_info"]
+        # student_info.street = kw["street"]
+        # student_info.zip = kw["zip"]
+        # student_info.permanent_district_id = kw["permanent_district_id"]
+        # student_info.country_id = kw["country_id"]
+        # student_info.present_street = kw["present_street"]
+        # student_info.present_zip = kw["present_zip"]
+        # student_info.present_district_id = kw["present_district_id"]
+        # student_info.present_country_id = kw["present_country_id"]
+        # student_info.is_permanent_present_address_same = kw["is_permanent_present_address_same"]
+        # student_info.whatsapp_number = kw["whatsapp_number"]
+        # student_info.social_network = kw["social_network"]
+        # student_info.nationality = kw["nationality"]
+        # student_info.national_id_no = kw["national_id_no"]
+        # student_info.passport_no = kw["passport_no"]
+        # student_info.visa_no = kw["visa_no"]
+        # student_info.nationalID = kw["nationalID"]
+        # student_info.visa_no = kw["visa_no"]
+        # student_info.visa_expire_date = kw["visa_expire_date"]
+        # student_info.father_name = kw["father_name"]
+        # student_info.father_contact_number = kw["father_contact_number"]
+        # student_info.father_email = kw["father_email"]
+        # student_info.father_national_id = kw["father_national_id"]
+        # student_info.father_passport = kw["father_passport"]
+        # student_info.father_birthday = kw["father_birthday"]
+        # student_info.father_age = kw["father_age"]
+        # student_info.father_occupation = kw["father_occupation"]
+        # student_info.father_designation = kw["father_designation"]
+        # student_info.father_annual_income = kw["father_annual_income"]
+        # student_info.mother_name = kw["mother_name"]
+        # student_info.mother_contact_number = kw["mother_contact_number"]
+        # student_info.mother_email = kw["mother_email"]
+        # student_info.mother_national_id = kw["mother_national_id"]
+        # student_info.mother_passport = kw["mother_passport"]
+        # student_info.mother_birthday = kw["mother_birthday"]
+        # student_info.mother_age = kw["mother_age"]
+        # student_info.mother_occupation = kw["mother_occupation"]
+        # student_info.mother_designation = kw["mother_designation"]
+        # student_info.local_guardian_name = kw["local_guardian_name"]
+        # student_info.local_guardian_email = kw["local_guardian_email"]
+        # student_info.local_guardian_contact_number = kw["local_guardian_contact_number"]
+        # student_info.local_guardian_national_id = kw["local_guardian_national_id"]
+        # student_info.local_guardian_passport = kw["local_guardian_passport"]
+        # student_info.local_guardian_relation = kw["local_guardian_relation"]
+        # student_info.local_guardian_street = kw["local_guardian_street"]
+        # student_info.local_guardian_zip = kw["local_guardian_zip"]
+        # student_info.local_guardian_state_id = kw["local_guardian_state_id"]
+        # student_info.local_guardian_country_id = kw["local_guardian_country_id"]
        
 
 
         return request.redirect('/my/admission/online/applications/list')
 
 
-    # @http.route('/my/admission/online/applications/list/form', type='http', auth='user', website=True)
-    # def my_student_admission_list_route(self, **kw):
-    #     user_id = request.env.context.get('uid')
-    #     kw.update({"user_id": user_id})
-    #     if request.httprequest.method == 'POST':
-    #     # Handle POST request to update data
-    #         student = request.env['se.application'].sudo().browse(user_id)
-    #         student.update(kw)
-    #         return http.redirect_with_hash('/my/admission/online/applications/list/form')
-    #     else:
-    #     # Handle GET request to display form
-    #         student_info = http.request.env['se.application'].search([])
-    #         return request.render("smartedu_portal.se_student_admission_template", {
-    #         'student_info': student_info,
-    #     })
+   
 
 
 
 # Update
 
-# @http.route('/my/admission/online/applications/list/form', type='http', auth='user', website=True)
-# def my_student_admission_form_route(self, **kw):
-#     if request.httprequest.method == 'POST':
-#         # Handle POST request to update data
-#         student_id = int(kw.get('student_id'))
-#         student = request.env['se.application'].sudo().browse(student_id)
-#         student.update(kw)
-#         return http.redirect_with_hash('/my/admission/online/applications/list/form')
-#     else:
-#         # Handle GET request to display form
-#         student_info = request.env['se.application'].search([])
-#         return request.render("smartedu_portal.se_student_admission_template", {
-#             'student_info': student_info,
-#         })
+# @http.route('/my/admission/online/applications/update', auth='public', website=True)
+# def update(self, **kw):
+#     # Get the ID of the student application to be updated
+#     student_id = int(kw.get('student_id'))
 
+#     # Retrieve the student application record and update the fields
+#     student = request.env['se.application'].sudo().browse(student_id)
+#     student.first_name = kw.get('first_name', False)
+#     student.middle_name = kw.get('middle_name', False)
+#     student.last_name = kw.get('last_name', False)
+
+#     # Update the many-to-one field (batch_id)
+#     batch_id = int(kw.get('batch_id', False))
+#     if batch_id:
+#         batch = request.env['se.batch'].sudo().browse(batch_id)
+#         student.batch_id = batch
+
+#     # Update the selection field (student_type_id)
+#     student_type_id = int(kw.get('student_type_id', False))
+#     if student_type_id:
+#         student_type = request.env['se.student.type'].sudo().browse(
+#             student_type_id)
+#         student.student_type_id = student_type
+
+#     # Redirect to the updated student application record
+#     return http.redirect_with_hash('/my/admission/online/applications/list/form#student_id=%d' % student_id)
 
 # Create Data From Portal to Backend
 
--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,9 @@
 
 from ...smartedu_core.models import se_student
 from ...smartedu_core.models import se_application
+from ...smartedu_core.models import se_batch
+from ...smartedu_core.models import se_student_type
+from ...smartedu_core.models import se_education_shift
+from ...smartedu_core.models import se_semester
+from ...smartedu_core.models import se_semester_type
 from . import models
